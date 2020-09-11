2f5c1b1df569a384714f63c0d19f2907f93a9f18
Bob Rubbens <bobrubbens@gmail.com>
2020-09-11 09:51:31 +0200

    Add the actual file

---
+++ examples/case-studies/Hagrid-Key-Simplified.java
@@ -0,0 +1,264 @@
+/**
+ * Simplified Java version of the HAGRID OpenPGP server
+ *( https://gitlab.com/hagrid-keyserver ). Manages the access to public keys.
+ *
+ * Adapted for VerCors, from: https://github.com/KeYProject/verifythis-ltc-2020/blob/master/simplified/Keyserver.java
+ */
+final public class Keyserver {
+    private int MAXUSERS;
+    private int[] emails;
+    private int[] keys;
+    private int[] codes;
+    private int[] unconfirmedKeys;
+    private int[] requestType;
+    private int count;
+
+    private int REQUESTTYPE_ADD;
+    private int REQUESTTYPE_REMOVE;
+
+    //@ ensures inv() ** Perm(REQUESTTYPE_ADD, read) ** Perm(REQUESTTYPE_REMOVE, read);
+    public Keyserver() {
+        MAXUSERS = 1024;
+        emails = new int[MAXUSERS];
+        keys = new int[MAXUSERS];
+        codes = new int[MAXUSERS];
+        unconfirmedKeys = new int[MAXUSERS];
+        requestType = new int[MAXUSERS];
+        count = 0;
+
+        REQUESTTYPE_ADD = 0;
+        REQUESTTYPE_REMOVE = 1;
+
+        //@ fold inv();
+    }
+
+    /*@
+    inline resource inv() =
+        // Positive number of max users
+        Perm(MAXUSERS, read) ** 0 < MAXUSERS
+        // All arrays are non-null and have the same length (the # users)
+        ** Perm(emails, read) ** emails != null ** emails.length == MAXUSERS ** Perm(emails[*], write)
+        ** Perm(keys, read) ** keys != null ** keys.length == MAXUSERS ** Perm(keys[*], write)
+        ** Perm(codes, read) ** codes != null ** codes.length == MAXUSERS ** Perm(codes[*], write)
+        ** Perm(unconfirmedKeys, read) ** unconfirmedKeys != null ** unconfirmedKeys.length == MAXUSERS ** Perm(unconfirmedKeys[*], write)
+        ** Perm(requestType, read) ** requestType != null ** requestType.length == MAXUSERS ** Perm(requestType[*], write)
+        // Number of users is bounded
+        ** Perm(count, write) ** 0 <= count ** count <= MAXUSERS
+        // Emails are unique
+        ** (\forall int i = 0 .. count;
+            (\forall int j = 0 .. count; i != j ==> emails[i] != emails[j]))
+        ;
+     */
+
+    /**
+     * Returns the array index where the info of the user with the specified
+     * id/email is stored.
+     *
+     * @param id
+     *            the id/email of the user
+     * @return the array index where user info is stored
+     */
+    //@ context_everywhere inv();
+    //@ ensures -1 <= \result && \result < count;
+    //@ ensures \result == -1 ==> (\forall int i; 0 <= i && i < count; emails[i] != id);
+    //@ ensures \result >= 0 ==> (emails[\result] == id && \result < count);
+    private int posOfId(int id) {
+        //@ loop_invariant 0 <= i && i <= count;
+        //@ loop_invariant (\forall int k = 0 .. i; emails[k] != id);
+        for(int i = 0; i < count;  i++) {
+            if(emails[i] == id) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Returns the key with the specified email address.
+     *
+     * @param id
+     *            the id/email of the user
+     * @return the key of the user
+     * @throws Exception
+     *             if the specified user id/email does not exist
+     */
+    /* public normal_behaviour
+        requires (\exists int i; 0 <= i && i < count; emails[i] == id);
+        ensures (\exists int i; 0 <= i && i < count; \result == keys[i] && emails[i] == id);
+        assignable \strictly_nothing;
+       also
+        public exceptional_behavior
+        requires (\forall int i; 0 <= i && i < emails.length; emails[i] != id);
+        signals (Exception e) true;
+      */
+    //@ context_everywhere inv();
+    /*@ ensures (\exists int i; 0 <= i && i < count; emails[i] == id)
+            ==> (\exists int i; 0 <= i && i < count; \result == keys[i] && emails[i] == id); */
+    //@ signals (Exception e) (\forall int i; 0 <= i && i < count; emails[i] != id);
+    public int get(int id) throws Exception {
+        int pos = posOfId(id);
+        if (pos >= 0) {
+            return keys[pos];
+        } else {
+            throw new Exception();
+        }
+    }
+
+    /**
+     * Stores request to add the given key for the specified user. The key still
+     * needs to be confirmed with {@link #addConfirm(int, int)}. Does nothing if
+     * the specified user does not exist.
+     *
+     * @param id
+     *            id the id/email of the user
+     * @param pkey
+     *            the key to be added after confirmation
+     * @return the array index where the key will be stored
+     */
+    /* public normal_behaviour
+        requires count < MAXUSERS;
+        ensures 0 <= \result;
+        ensures count == \old(count) && \result < count
+            ||  count == \old(count) + 1 && \result == count - 1;
+        ensures emails[\result] == id && unconfirmedKeys[\result] == pkey && codes[\result]>0;
+        ensures requestType[\result] == REQUESTTYPE_ADD;
+        // preservation of the other entries
+        ensures (\forall int i; 0<=i && i<count;
+                    (emails[i] == (i == \result ? id : \old(emails[i])))
+                 && (unconfirmedKeys[i] == (i == \result ? pkey : \old(unconfirmedKeys[i])))
+                 && (i != \result ==> (codes[i] == \old(codes[i])))
+                 && (i != \result ==> (requestType[i] == \old(requestType[i]))));
+        assignable emails[*], unconfirmedKeys[*], codes[*], requestType[*], count;
+      */
+    //@ context inv();
+    //@ requires count < MAXUSERS;
+    //@ ensures 0 <= \result;
+    public int addRequest(int id, int pkey) {
+        int pos = posOfId(id);
+
+        if(pos < 0) {
+            pos = count;
+            count ++;
+        }
+
+        emails[pos] = id;
+        codes[pos] = 1; // TODO: Random positive number?
+        unconfirmedKeys[pos] = pkey;
+        requestType[pos] = REQUESTTYPE_ADD;
+        return pos;
+    }
+
+    /**
+     * Stores the key previously supplied to {@link #addRequest(int, int)} if
+     * the given code matches the secret confirmation code generated in
+     * {@code addRequest} for the given user. If it does not match: does
+     * nothing.
+     *
+     * @param id
+     *            the id/email of the user
+     * @param code
+     *            the confirmation code for the add operation
+     * @return the index where the key is stored, or -1 if nothing is done.
+     */
+    /* public normal_behaviour
+        requires code > 0 && (\exists int i; 0 <= i && i < count; (emails[i] == id && codes[i] == code && requestType[i] == REQUESTTYPE_ADD));
+        ensures 0 <= \result;
+        ensures emails[\result] == id && keys[\result] == \old(unconfirmedKeys[\result]) && codes[\result]==0;
+        // preservation of the other entries
+        ensures (\forall int i; 0<=i && i<count; (i != \result ==>
+                    (keys[i] == \old(keys[i])) && (codes[i] == \old(codes[i]))));
+        assignable keys[*], codes[*];
+       also
+       public normal_behaviour
+        requires code <= 0 || !(\exists int i; 0 <= i && i < count; (emails[i] == id && codes[i] == code && requestType[i] == REQUESTTYPE_ADD));
+        ensures \result == -1;
+        assignable \strictly_nothing;
+      */
+    public int addConfirm(int id, int code) {
+        int pos = posOfId(id);
+
+        if(pos >= 0 && code > 0 && code == codes[pos] && requestType[pos] == REQUESTTYPE_ADD) {
+            // code confirmed, store key
+            keys[pos] = unconfirmedKeys[pos];
+            codes[pos] = 0;
+        } else {
+            pos = -1;
+        }
+
+        return pos;
+    }
+
+    /**
+     * Stores request to remove the given key for the specified user. The
+     * removal still needs to be confirmed with {@link #delConfirm(int, int)}.
+     * Does nothing if the specified user does not exist.
+     *
+     * @param id
+     *            the id/email of the user
+     * @return the array index where the key will be stored. -1 if the user does
+     *         not exist
+     */
+    /* public normal_behaviour
+        requires (\exists int i; 0 <= i && i < count; emails[i] == id);
+        ensures requestType[\result] == REQUESTTYPE_REMOVE;
+        ensures (\forall int i; 0<=i && i<count;
+                    (emails[i] == (i == \result ? id : \old(emails[i])))
+                 && (i != \result ==> (codes[i] == \old(codes[i])))
+                 && (i != \result ==> (requestType[i] == \old(requestType[i]))));
+        ensures codes[\result] > 0;
+        assignable codes[*], requestType[*];
+       also
+       public normal_behaviour
+        requires !(\exists int i; 0 <= i && i < count; emails[i] == id);
+        ensures \result == -1;
+        assignable \strictly_nothing;
+      */
+    public int delRequest(int id) {
+        int pos = posOfId(id);
+        if(pos >= 0) {
+            if(count > 0 && pos != count) {
+                codes[pos] = 1; // Random positive number?
+                requestType[pos] = REQUESTTYPE_REMOVE;
+            }
+        }
+
+        return pos;
+    }
+
+    /**
+     * Removes the key as previously requested in {@link #delRequest(int)} if
+     * the given code matches the secret confirmation code generated in
+     * {@code delRequest} for the given user. Does nothing if there is no match.
+     *
+     * @param id
+     *            the id/email of the user
+     * @param code
+     *            the confirmation code for the removal operation
+     */
+    /* public normal_behaviour
+        requires code > 0 && (\exists int i; 0 <= i && i < count; (emails[i] == id && codes[i] == code && requestType[i] == REQUESTTYPE_REMOVE));
+        ensures count == \old(count) - 1;
+        ensures !(\exists int i; 0 <= i && i < count; emails[i] == id);
+        ensures (\forall int e; (\forall int k; e != id;
+                       \old((\exists int i; 0 <= i && i < count; emails[i] == e && keys[i] == k))
+                  <==> (\exists int i; 0 <= i && i < count; emails[i] == e && keys[i] == k)));
+        assignable emails[*], keys[*], count;
+       also
+       public normal_behaviour
+        requires code <= 0 || !(\exists int i; 0 <= i && i < count; (emails[i] == id && codes[i] == code && requestType[i] == REQUESTTYPE_REMOVE));
+        assignable \strictly_nothing;
+      */
+    public void delConfirm(int id, int code) {
+
+        int pos = posOfId(id);
+        if(pos >= 0 && code > 0 && code == codes[pos] && requestType[pos] == REQUESTTYPE_REMOVE) {
+            //code confirmed, remove key
+            count --;
+            if(count > 0 && pos != count) {
+                emails[pos] = emails[count];
+                keys[pos] = keys[count];
+            }
+        }
+    }
+}
